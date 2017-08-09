359f42db1b1848a7557c8223f2b030b8156c9d08
shan2new <shantanu.sinha@navyuginfo.com>
2017-08-09 21:43:46 +0530

    Enough for Today

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
 
-	def postOrder(item, id)
+	$id = 1
+	def postOrder(item, id1)
 		if item == "." or item == ".."
 			return
 		end
@@ -8,8 +9,10 @@ class ApplicationController < ActionController::API
 		path = "#{item}/"
 		tpath = "#{item}"
 		parent = tpath.split("/")[-2]
+		%x{echo node: "#{item} #{parent} #{id1} #{$id}" >> output}
 		temp = tpath.split("/")[-1]
 		if File.directory?(item)
+			$id = $id + 1
 			ptype = "directory"
 			Dirdetail.create(name: temp, path: path, owner: owner, ptype: ptype, parent: parent)
 			Dir.foreach(item) do |itr|
@@ -17,22 +20,23 @@ class ApplicationController < ActionController::API
 					@test = 0
 				else
 					x = "#{item}/#{itr}"
-					postOrder(x, id + 1)
+					postOrder(x, "#{$id}".to_i)
 				end
 			end
 		else
-			%x{echo file: "#{item} #{parent} #{id}" >> output}
-			@x = Filedetail.new(name: temp, path: path, owner: owner, ptype: ptype, parent: parent,  dirdetail_id: id)
+			%x{echo file: "#{id1}" >> output}
+			@x = Filedetail.new(name: temp, path: path, owner: owner, ptype: ptype, parent: parent,  dirdetail_id: "#{id1}")
 			@x.save
-			%x{echo file: "#{@x.errors.full_messages}" >> output}
+			%x{echo errors: #{@x.errors.full_messages}}
 		end
 	end
 
 	def update
+		Dirdetail.create(name: "root", path: "/", owner: "shan2new", ptype: "files", parent: "self")
 		Dir.foreach(Dir.pwd) do |item|
-			postOrder(item, 2)		
+			postOrder(item, $id)		
 		end
-		redirect_to "localhost:3000/dirdetails/?path=app/"
+		redirect_to filedetails_path
 	end	
 
 end
